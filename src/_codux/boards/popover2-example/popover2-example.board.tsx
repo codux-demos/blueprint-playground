dd8d8938a230710ff53e53ce220ae5a0cbec6e18
Sagiv Dayan <67858097+TheAlmightyCrumb@users.noreply.github.com>
2023-07-10 14:55:29 +0300

    chore: create-react-app -> vite migration (#14)

--- src/_codux/boards/popover2-example/popover2-example.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Popover2Example } from '../../../components/popover2-example/popover2-example';
-
-export default createBoard({
-    name: 'Popover2 Example',
-    Board: () => <Popover2Example />,
-});

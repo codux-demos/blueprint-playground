c9f0b919eabf49e4c7d88315b7154b2a9e2025e5
Avi Vahl <avi.vahl@wix.com>
2024-08-26 12:27:28 +0300

    deps: upgrade to react@18 use @blueprintjs/datetime2 to support v18

--- src/components/datetime-example/datetime-example.tsx
+++ src/components/datetime-example/datetime-example.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import { DatePicker } from '@blueprintjs/datetime';
+import { DatePicker3 } from '@blueprintjs/datetime2';
 
 import { Example } from '../example/example';
 import styles from './datetime-example.module.scss';
@@ -11,7 +11,7 @@ export interface DatetimeExampleProps {
 export const DatetimeExample = ({ className }: DatetimeExampleProps) => {
     return (
         <Example header="Datetime Example" className={classNames(styles.root, className)}>
-            <DatePicker className={styles.datepicker} timePrecision="minute" />
+            <DatePicker3 className={styles.datepicker} timePrecision="minute" />
         </Example>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 
 import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
+import '@blueprintjs/datetime2/lib/css/blueprint-datetime2.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import '@blueprintjs/select/lib/css/blueprint-select.css';
 import '@blueprintjs/table/lib/css/table.css';
